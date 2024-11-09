ae4f22d19f40a55eeb6937bab30a6b85cc2027e9
saftarli <tofiqsefterli@gmail.com>
2024-11-10 01:50:59 +0400

    18 ci video

--- shop/products/routes.py
+++ shop/products/routes.py
@@ -168,3 +168,24 @@ def updateproduct(id):
     form.discription.data = product.desc
     
     return render_template('products/updateproduct.html', form=form, brands=brands,categories=categories,product=product)
+
+
+@app.route('/deleteproduct/<int:id>', methods = ["POST"])
+def deleteproduct(id):
+    product = Addproduct.query.get_or_404(id)
+    if request.method == "POST":
+        if request.files.get('image_1'):
+            try:
+                os.unlink(os.path.join(current_app.root_path, 'static/images/' + product.image_1))
+                os.unlink(os.path.join(current_app.root_path, 'static/images/' + product.image_2))
+                os.unlink(os.path.join(current_app.root_path, 'static/images/' + product.image_3))
+            except Exception as e:
+                print(e)
+                
+        db.session.delete(product)
+        db.session.commit()
+        flash(f'The product {product.name} was deleted from your record', 'success')
+        return redirect(url_for('home'))
+    flash(f'Cant delete the product', 'danger')
+    return redirect('/home')        
+                
