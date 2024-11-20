3490dd3082fb1c0cc1a675ebe3b25bd325aceb86
saftarli <tofiqsefterli@gmail.com>
2024-11-20 13:20:07 +0400

    35 ci video

--- shop/__init__.py
+++ shop/__init__.py
@@ -5,7 +5,7 @@ from flask_uploads import IMAGES, UploadSet, configure_uploads, patch_request_cl
 import os
 from flask_msearch import Search
 from flask_login import LoginManager
-
+from flask_migrate import Migrate
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app= Flask(__name__)
@@ -22,6 +22,16 @@ bcrypt = Bcrypt(app)
 search = Search(db=db)
 search.init_app(app)
 
+migrate = Migrate(app, db)
+with app.app_context():
+    if db.engine.url.drivername == "sqlite":
+        migrate.init_app(app,db,render_as_batch=True)
+    else:
+        migrate.init_app(app,db)
+        
+        
+        
+        
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'customerLogin'
